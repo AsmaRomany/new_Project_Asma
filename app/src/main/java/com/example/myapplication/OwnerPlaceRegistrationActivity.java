80cafd4e380d1f152e56f81c461553147a52d811
jerusalem <jerusalem@DESKTOP-DAIF0TH>
2023-07-17 20:40:09 +0300

    nada's commit

--- app/src/main/java/com/example/myapplication/OwnerPlaceRegistrationActivity.java
+++ app/src/main/java/com/example/myapplication/OwnerPlaceRegistrationActivity.java
@@ -15,6 +15,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -89,6 +91,8 @@ private void savePlaceDetails(String ownerId) {
     // Create a new document in the "Places" collection
     DocumentReference placeRef = db.collection("Places").document();
 
+
+
     // Create a HashMap to store the place details
     Map<String, Object> placeInfo = new HashMap<>();
     placeInfo.put("name", placeName);
@@ -97,6 +101,7 @@ private void savePlaceDetails(String ownerId) {
     placeInfo.put("ownerId", ownerId); // Associate the owner with the place
     placeInfo.put("placeType", placeType); // Add the selected place type
 
+
     // Set the place information in the Firestore document
     placeRef.set(placeInfo)
             .addOnSuccessListener(new OnSuccessListener<Void>() {
@@ -104,17 +109,41 @@ private void savePlaceDetails(String ownerId) {
                 public void onSuccess(Void aVoid) {
                     // Document successfully written
                     Toast.makeText(OwnerPlaceRegistrationActivity.this, "Place details saved successfully.", Toast.LENGTH_SHORT).show();
+
+                  // Compare the placeType and add the name of the place to the Realtime Database
+                                      if (placeType.equals("salon") || placeType.equals("Resturant") || placeType.equals("StudyPlaces") || placeType.equals("Dorms")|| placeType.equals("Supermarket")|| placeType.equals("DryClean")) {
+                                          DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child(placeType);
+                                           // Get the generated Firestore document ID
+                                          DatabaseReference placeTypeRef = databaseReference.child(placeType +placeName);
+
+                                                           placeTypeRef.setValue(placeName)
+                                                                          .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                                              @Override
+                                                                              public void onSuccess(Void aVoid) {
+                                                                                  // Place name added to the Realtime Database
+                                                                                  Toast.makeText(OwnerPlaceRegistrationActivity.this, "Place name added to the Realtime Database.", Toast.LENGTH_SHORT).show();
+                                                                              }
+                                                                          })
+                                                                          .addOnFailureListener(new OnFailureListener() {
+                                                                              @Override
+                                                                              public void onFailure(@NonNull Exception e) {
+                                                                                  // Error adding place name to the Realtime Database
+                                                                                  Toast.makeText(OwnerPlaceRegistrationActivity.this, "Failed to add place name to the Realtime Database.", Toast.LENGTH_SHORT).show();
+                                                                              }
+                                                                          });
+                                                              }                
                     // Optionally, you can navigate to another activity or perform further actions
                     finish(); // Finish the activity after saving the place details
                 }
             })
-            .addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    // Error writing document
-                    Toast.makeText(OwnerPlaceRegistrationActivity.this, "Failed to save place details.", Toast.LENGTH_SHORT).show();
-                }
-            });
+
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Error writing document
+                                Toast.makeText(OwnerPlaceRegistrationActivity.this, "Failed to save place details.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
 }
 
-}
